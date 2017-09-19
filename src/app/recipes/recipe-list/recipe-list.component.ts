8402e818bad0f0e7c2da1a9d50847cb62f9196af
marcos_lomboglia <marcos_lomboglia>
2017-09-19 16:32:42 +0100

    commit two

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe','A dec Recipe','https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556_960_720.jpg'),
+    new Recipe('A test Recipe','A dec Recipe','https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556_960_720.jpg')
+  ];
 
   constructor() { }
 
